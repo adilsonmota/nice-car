1eb7a05400bb9df5da9941990ea0f7e57d57dde6
Adilson Mota <adilsonba@gmail.com>
2020-05-18 09:53:57 -0300

    Demais telas Criadas

---
+++ src/main/java/control/BasicView.java
@@ -0,0 +1,21 @@
+package control;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import entities.Vehicle;
+
+@RequestScoped
+@ManagedBean(name = "BasicView")
+public class BasicView  {
+     
+    private List<Vehicle> listVehicles;
+    
+     
+    public List<Vehicle> getCars() {
+        return listVehicles;
+    }
+ 
+}
